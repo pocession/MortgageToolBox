71fe800283cfab9953f37190af2a36832c3bad75
TsungHan Hsieh <tsunghan.hsieh@oist.jp>
2023-11-22 22:55:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-var interestTaiwan = parseFloat(document.getElementById('interestTaiwan').value);
-var interestEurope = parseFloat(document.getElementById('interestEurope').value);
-var loanAmountEuros = parseFloat(document.getElementById('loanAmountEuros').value);
-var mortgageYears = parseInt(document.getElementById('mortgageYears').value);
-var totalMonth = mortgageYears * 12;
-var monthlyInterestRateTaiwan = interestTaiwan / 100 / 12
-var monthlyInterestRateEuro = interestEurope / 100 / 12
-var wireFee = parseInt(document.getElementById('wireFee').value);
+
 var projectedLowExchangeRate = [33.019476, 32.733171, 32.292004, 32.105310, 31.933638, 31.773857]
 var projectedHighExchangeRate = [35.475105, 35.759649, 35.994239, 36.200764, 36.387458, 36.559130]
 var currentExchangeRate = 34.66
 
 
 function calculateSavings() {
+    var interestTaiwan = parseFloat(document.getElementById('interestTaiwan').value);
+    var interestEurope = parseFloat(document.getElementById('interestEurope').value);
+    var loanAmountEuros = parseFloat(document.getElementById('loanAmountEuros').value);
+    var mortgageYears = parseInt(document.getElementById('mortgageYears').value);
+    var totalMonth = mortgageYears * 12;
+    var monthlyInterestRateTaiwan = interestTaiwan / 100 / 12
+    var monthlyInterestRateEuro = interestEurope / 100 / 12
+    var wireFee = parseInt(document.getElementById('wireFee').value);
 
     if (!isNaN(interestTaiwan) && !isNaN(interestEurope) && !isNaN(loanAmountEuros) && !isNaN(mortgageYears)) {
 
@@ -41,6 +42,15 @@ function calculateSavings() {
 
 function calculateProjection() {
 
+    var interestTaiwan = parseFloat(document.getElementById('interestTaiwan').value);
+    var interestEurope = parseFloat(document.getElementById('interestEurope').value);
+    var loanAmountEuros = parseFloat(document.getElementById('loanAmountEuros').value);
+    var mortgageYears = parseInt(document.getElementById('mortgageYears').value);
+    var totalMonth = mortgageYears * 12;
+    var monthlyInterestRateTaiwan = interestTaiwan / 100 / 12
+    var monthlyInterestRateEuro = interestEurope / 100 / 12
+    var wireFee = parseInt(document.getElementById('wireFee').value);
+    
     if (!isNaN(interestTaiwan) && !isNaN(interestEurope) && !isNaN(loanAmountEuros) && !isNaN(mortgageYears)) {
         var monthlyPaymentEuro = (loanAmountEuros * monthlyInterestRateEuro) / (1 - Math.pow(1 + monthlyInterestRateEuro, -totalMonth));
         var monthlyPaymentEuroPlusWiring = (monthlyPaymentEuro + wireFee).toFixed(2);
@@ -52,19 +62,19 @@ function calculateProjection() {
         document.getElementById('fifthExchangeRate').innerHTML = projectedLowExchangeRate[4].toFixed(2) + " - " + projectedHighExchangeRate[4].toFixed(2);
         document.getElementById('sixthExchangeRate').innerHTML = projectedLowExchangeRate[5].toFixed(2) + " - " + projectedHighExchangeRate[5].toFixed(2);
 
-        document.getElementById('firstProjectedPayment').innerHTML = (projectedLowExchangeRate[0]*monthlyPaymentEuroPlusWiring).toFixed(2) + " - " + (projectedHighExchangeRate[0]*monthlyPaymentEuroPlusWiring).toFixed(2)
-        document.getElementById('secondProjectedPayment').innerHTML = (projectedLowExchangeRate[1]*monthlyPaymentEuroPlusWiring).toFixed(2) + " - " + (projectedHighExchangeRate[1]*monthlyPaymentEuroPlusWiring).toFixed(2)
-        document.getElementById('thirdProjectedPayment').innerHTML = (projectedLowExchangeRate[2]*monthlyPaymentEuroPlusWiring).toFixed(2) + " - " + (projectedHighExchangeRate[2]*monthlyPaymentEuroPlusWiring).toFixed(2)
-        document.getElementById('fourthProjectedPayment').innerHTML = (projectedLowExchangeRate[3]*monthlyPaymentEuroPlusWiring).toFixed(2) + " - " + (projectedHighExchangeRate[3]*monthlyPaymentEuroPlusWiring).toFixed(2)
-        document.getElementById('fifthProjectedPayment').innerHTML = (projectedLowExchangeRate[4]*monthlyPaymentEuroPlusWiring).toFixed(2) + "  - " + (projectedHighExchangeRate[4]*monthlyPaymentEuroPlusWiring).toFixed(2)
-        document.getElementById('sixthProjectedPayment').innerHTML = (projectedLowExchangeRate[5]*monthlyPaymentEuroPlusWiring).toFixed(2) + " - " + (projectedHighExchangeRate[5]*monthlyPaymentEuroPlusWiring).toFixed(2)
+        document.getElementById('firstProjectedPayment').innerHTML = (projectedLowExchangeRate[0] * monthlyPaymentEuroPlusWiring).toFixed(2) + " - " + (projectedHighExchangeRate[0] * monthlyPaymentEuroPlusWiring).toFixed(2)
+        document.getElementById('secondProjectedPayment').innerHTML = (projectedLowExchangeRate[1] * monthlyPaymentEuroPlusWiring).toFixed(2) + " - " + (projectedHighExchangeRate[1] * monthlyPaymentEuroPlusWiring).toFixed(2)
+        document.getElementById('thirdProjectedPayment').innerHTML = (projectedLowExchangeRate[2] * monthlyPaymentEuroPlusWiring).toFixed(2) + " - " + (projectedHighExchangeRate[2] * monthlyPaymentEuroPlusWiring).toFixed(2)
+        document.getElementById('fourthProjectedPayment').innerHTML = (projectedLowExchangeRate[3] * monthlyPaymentEuroPlusWiring).toFixed(2) + " - " + (projectedHighExchangeRate[3] * monthlyPaymentEuroPlusWiring).toFixed(2)
+        document.getElementById('fifthProjectedPayment').innerHTML = (projectedLowExchangeRate[4] * monthlyPaymentEuroPlusWiring).toFixed(2) + "  - " + (projectedHighExchangeRate[4] * monthlyPaymentEuroPlusWiring).toFixed(2)
+        document.getElementById('sixthProjectedPayment').innerHTML = (projectedLowExchangeRate[5] * monthlyPaymentEuroPlusWiring).toFixed(2) + " - " + (projectedHighExchangeRate[5] * monthlyPaymentEuroPlusWiring).toFixed(2)
 
-        document.getElementById('firstProjectedLoss').innerHTML = (projectedLowExchangeRate[0]*monthlyPaymentEuroPlusWiring - currentExchangeRate*monthlyPaymentEuroPlusWiring).toFixed(2) + " - " + (projectedHighExchangeRate[0]*monthlyPaymentEuroPlusWiring - currentExchangeRate*monthlyPaymentEuroPlusWiring).toFixed(2)
-        document.getElementById('secondProjectedLoss').innerHTML = (projectedLowExchangeRate[1]*monthlyPaymentEuroPlusWiring - currentExchangeRate*monthlyPaymentEuroPlusWiring).toFixed(2) + " - " + (projectedHighExchangeRate[0]*monthlyPaymentEuroPlusWiring - currentExchangeRate*monthlyPaymentEuroPlusWiring).toFixed(2)
-        document.getElementById('thirdProjectedLoss').innerHTML = (projectedLowExchangeRate[2]*monthlyPaymentEuroPlusWiring - currentExchangeRate*monthlyPaymentEuroPlusWiring).toFixed(2) + " - " + (projectedHighExchangeRate[0]*monthlyPaymentEuroPlusWiring - currentExchangeRate*monthlyPaymentEuroPlusWiring).toFixed(2)
-        document.getElementById('fourthProjectedLoss').innerHTML = (projectedLowExchangeRate[3]*monthlyPaymentEuroPlusWiring - currentExchangeRate*monthlyPaymentEuroPlusWiring).toFixed(2) + " - " + (projectedHighExchangeRate[0]*monthlyPaymentEuroPlusWiring - currentExchangeRate*monthlyPaymentEuroPlusWiring).toFixed(2)
-        document.getElementById('fifthProjectedLoss').innerHTML = (projectedLowExchangeRate[4]*monthlyPaymentEuroPlusWiring - currentExchangeRate*monthlyPaymentEuroPlusWiring).toFixed(2) + " - " + (projectedHighExchangeRate[0]*monthlyPaymentEuroPlusWiring - currentExchangeRate*monthlyPaymentEuroPlusWiring).toFixed(2)
-        document.getElementById('sixthProjectedLoss').innerHTML = (projectedLowExchangeRate[5]*monthlyPaymentEuroPlusWiring - currentExchangeRate*monthlyPaymentEuroPlusWiring).toFixed(2) + " - " + (projectedHighExchangeRate[0]*monthlyPaymentEuroPlusWiring - currentExchangeRate*monthlyPaymentEuroPlusWiring).toFixed(2)
+        document.getElementById('firstProjectedLoss').innerHTML = (projectedLowExchangeRate[0] * monthlyPaymentEuroPlusWiring - currentExchangeRate * monthlyPaymentEuroPlusWiring).toFixed(2) + " - " + (projectedHighExchangeRate[0] * monthlyPaymentEuroPlusWiring - currentExchangeRate * monthlyPaymentEuroPlusWiring).toFixed(2)
+        document.getElementById('secondProjectedLoss').innerHTML = (projectedLowExchangeRate[1] * monthlyPaymentEuroPlusWiring - currentExchangeRate * monthlyPaymentEuroPlusWiring).toFixed(2) + " - " + (projectedHighExchangeRate[0] * monthlyPaymentEuroPlusWiring - currentExchangeRate * monthlyPaymentEuroPlusWiring).toFixed(2)
+        document.getElementById('thirdProjectedLoss').innerHTML = (projectedLowExchangeRate[2] * monthlyPaymentEuroPlusWiring - currentExchangeRate * monthlyPaymentEuroPlusWiring).toFixed(2) + " - " + (projectedHighExchangeRate[0] * monthlyPaymentEuroPlusWiring - currentExchangeRate * monthlyPaymentEuroPlusWiring).toFixed(2)
+        document.getElementById('fourthProjectedLoss').innerHTML = (projectedLowExchangeRate[3] * monthlyPaymentEuroPlusWiring - currentExchangeRate * monthlyPaymentEuroPlusWiring).toFixed(2) + " - " + (projectedHighExchangeRate[0] * monthlyPaymentEuroPlusWiring - currentExchangeRate * monthlyPaymentEuroPlusWiring).toFixed(2)
+        document.getElementById('fifthProjectedLoss').innerHTML = (projectedLowExchangeRate[4] * monthlyPaymentEuroPlusWiring - currentExchangeRate * monthlyPaymentEuroPlusWiring).toFixed(2) + " - " + (projectedHighExchangeRate[0] * monthlyPaymentEuroPlusWiring - currentExchangeRate * monthlyPaymentEuroPlusWiring).toFixed(2)
+        document.getElementById('sixthProjectedLoss').innerHTML = (projectedLowExchangeRate[5] * monthlyPaymentEuroPlusWiring - currentExchangeRate * monthlyPaymentEuroPlusWiring).toFixed(2) + " - " + (projectedHighExchangeRate[0] * monthlyPaymentEuroPlusWiring - currentExchangeRate * monthlyPaymentEuroPlusWiring).toFixed(2)
 
     } else {
         document.getElementById('firstExchangeRate').innerHTML = "Please enter valid values.";
@@ -113,6 +123,6 @@ function updateVisitorCount() {
 }
 
 // Call the updateVisitorCount function when the page loads
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     updateVisitorCount();
 });
